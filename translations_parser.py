f811d2887ad3ec2b1c9803fc219418d946c4f2c7
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-04-25 09:09:07 +0200

    feat: Parse stringsdict

--- translations_parser.py
+++ translations_parser.py
@@ -1,4 +1,5 @@
 import re
+import xml.etree.ElementTree as ElementTree
 
 class TranslationsParser:
     def parse(self, filename):
@@ -20,4 +21,29 @@ class StringsTranslationsParser(TranslationsParser):
 
 class StringsDictTranslationsParser(TranslationsParser):
     def parse(self, filename):
-        print("coucou")
+        xml_tree = ElementTree.parse(filename)
+        root_dict = xml_tree.find("dict")
+
+        data = {}
+
+        key_index = 0
+        while key_index + 1 < len(root_dict):
+            key = root_dict[key_index].text
+            dict_items = root_dict[key_index + 1]
+
+            strings_dict = dict_items.find("dict")
+            dict_index = 0
+            while dict_index + 1 < len(strings_dict):
+                plural_form = strings_dict[dict_index].text
+                if plural_form in ["NSStringFormatSpecTypeKey", "NSStringFormatValueTypeKey"]:
+                    dict_index += 2
+                    continue
+
+                value = strings_dict[dict_index + 1].text
+                data[f"{key}-{plural_form}"] = value
+
+                dict_index += 2
+
+            key_index += 2
+
+        return data
