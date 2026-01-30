124165d2f499e464e85a70606cdfc8dd56e0a417
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-01-30 10:51:46 +0100

    chore: Print correclty

--- src/import_loco/core/loco/platform_import.py
+++ src/import_loco/core/loco/platform_import.py
@@ -15,7 +15,7 @@ def import_translations(platform: Platform, resource_type: str) -> None:
     if resource_type not in platform.get_resource_types():
         raise ValueError(f"Resource type '{resource_type}' not supported by platform")
 
-    logger.debug("Starting import %s", resource_type)
+    print(f"Starting import {resource_type}")
 
     archive_path = _download_archive(platform, resource_type)
     print(f"(1/3) {resource_type} archive downloaded from Loco.")
